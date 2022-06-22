307641912ada0e54ec8eabbd0de6b9d00623ea33
jfr2102 <johannes@friedlein.eu>
2022-06-22 12:31:39 +0200

    add endpoint test

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var app = express();
 const port = 22666;
 app.get('/', function (req, res) {
    console.log(req.body)
-   res.send('Hello World');
+   res.send('Process ');
 })
 
-
+app.post("/add", function(req, res) {
+    console.log("add task")
+    console.log(req.body)
+    res.json({test: "newWorkListItem"});
+})
 app.listen(port, () => {
         console.log(`listening on port ${port}`);
 });  
