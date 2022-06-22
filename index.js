070a589a170d2683378cde9013de83e02ca17835
jfr2102 <johannes@friedlein.eu>
2022-06-22 12:52:31 +0200

    add CPEE Callback header

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 const port = 22666;
+
+app.use(cors())
+app.use(express.json())
+
 app.get('/', function (req, res) {
    console.log(req.body)
-   res.send('Process ');
+   res.send('Process');
 })
 
 app.post("/add", function(req, res) {
     console.log("add task")
     console.log(req.body)
+    res.set("CPEE-CALLBACK", true);
     res.json({test: "newWorkListItem"});
 })
 app.listen(port, () => {
