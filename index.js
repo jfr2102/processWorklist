640d34eec34b60704522ba6feb6719ad3c7904b2
jfr2102 <johannes@friedlein.eu>
2022-06-23 03:33:33 +0200

    access callback headers

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 var app = express();
+var axios = require("axios");
+
 const port = 22666;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', function (req, res) {
-   console.log(req.body)
-   res.send('Process');
-})
+app.get("/", (req, res) => {
+  console.log(req.body);
+  res.send("Process Worklist");
+});
 
-app.post("/add", function(req, res) {
-    console.log("add task")
-    console.log(req.body)
-    res.set("CPEE-CALLBACK", true);
-    res.json({test: "newWorkListItem"});
-})
+app.post("/add", (req, res) => {
+  console.log("add task");
+  console.log(req.headers);
+  console.log(req.body);
+  res.set("CPEE-CALLBACK", true);
+  res.json({ test: "newWorkListItem" });
+});
+///:id
+app.get("/callbacks/:id", (req, res) => {
+  console.log("callbacks: " + req.params.id);
+  axios
+    .get(`https://cpee.org/flow/engine/${req.params.id}/callbacks/`)
+    .then((response) => {
+      console.log(response.data);
+      res.json({ html: response.data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.listen(port, () => {
-        console.log(`listening on port ${port}`);
-});  
+  console.log(`listening on port ${port}`);
+});
