d40db8834eb44b5ab67dd031bd54fbb02df2eadd
Johannes Friedlein <johannes@friedlein.eu>
2022-06-29 00:30:05 +0200

    non relative path in routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 // import Box from "@mui/material/Box";
 import { Routes, Route } from "react-router-dom";
 function App() {
+  const baseUrl = "/~ge56wed/processWorklist/frontend/build/"
   return <div>
     <Routes>
-                    <Route strict path="/" element={<div><p>HOME</p></div>} />
-                    <Route path="/worklist" element={<div><p>WORKLIST</p></div>} />
+                    <Route strict path={baseUrl} element={<div><p>HOME</p></div>} />
+                    <Route path={baseUrl + "worklist"} element={<div><p>WORKLIST</p></div>} />
     </Routes>
 
   </div>;
